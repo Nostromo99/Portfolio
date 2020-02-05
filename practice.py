4091247003f6115c88ad9770746bea6cb3ef4a9f
Nostromo99 <60355083+Nostromo99@users.noreply.github.com>
2020-02-05 12:37:42 +0000

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,20 +1,25 @@
 import pickle
 import re
 import requests
-stuff_2=pickle.load(open("save.p","rb"))
-stuff={"AMZN":0,"TSLA":0,"TTWO":0,"DELL":0,"NVDA":0,"AMD":0}
-def update(dict):
+class Portfolio():
+    def __init__(self):
+        self.stuff={"AMZN":0,"TSLA":0,"TTWO":0,"DELL":0,"NVDA":0,"AMD":0}
+        try: pickle.load(open("save.p","rb"))
+        except FileNotFoundError:
+                self.update()
+        self.stuff=pickle.load(open("save.p", "rb"))
+    def update(self):
 
-    for item in dict.keys():
-        x=0
-        print(item)
-        res=requests.get("https://finance.yahoo.com/quote/"+item+"?p="+item)
-        x=re.search('"regularMarketPrice":{"raw":[0-9]*(\.[0-9]*)?',res.text)
-        y=float(re.search('[0-9].*',x.group()).group())
-        #z=re.search('[0-9].*',y)
-        dict[item]=y
-    return dict
-#stuff=update(stuff)
-print(stuff_2)
-pickle.dump(stuff,open("save.p","wb"))
-
+        for item in self.stuff.keys():
+            x=0
+            print(item)
+            res=requests.get("https://finance.yahoo.com/quote/"+item+"?p="+item)
+            x=re.search('"regularMarketPrice":{"raw":[0-9]*(\.[0-9]*)?',res.text)
+            y=float(re.search('[0-9].*',x.group()).group())
+            #z=re.search('[0-9].*',y)
+            self.stuff[item]=y
+            self.save()
+        return self.stuff
+    def save(self):
+        pickle.dump(self.stuff,open("save.p","wb"))
+test=Portfolio()
