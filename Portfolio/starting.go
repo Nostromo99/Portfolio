d459d36f449df4d582098f813b31723778649925
aaron <aaronjondorney@gmail.com>
2020-09-20 23:20:45 +0100

    implementing concurrency

--- Portfolio/starting.go
+++ Portfolio/starting.go
@@ -8,8 +8,11 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
+//add concurency
+//read in lines and create function to send get requests
 func main() {
 
 	tickers, err := os.Open("portfolio/tickers.txt")
@@ -18,44 +21,62 @@ func main() {
 	}
 	defer tickers.Close()
 	lines := bufio.NewScanner(tickers)
+	name := []string{}
+	count := 0
 	for lines.Scan() {
-		stock := lines.Text()
-		resp, err := http.Get("https://www.google.com/search?q=" + stock + "+stock&safe=strict&rlz=1C1CHBF_enIE784IE784&sxsrf=ALeKk00R7a6xf6UTpwxc_R23lq_m9yQx0A:1600457550056&gbv=1&sei=TgtlX_eLA9uM1fAP7MacsAc")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		output, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		outputstring := string(output)
-		position := strings.LastIndex(outputstring, "iBp4i")
-		value := string(outputstring[position+14 : position+25])
-		for i := 0; i < len(value); i++ {
-			if string(value[i]) == " " {
-				value = value[0:i]
-				break
-			}
-		}
-		fmt.Println(stock + ": " + string(value))
+		name = append(name, lines.Text())
+		count++
+		// stock := lines.Text()
+		// resp, err := http.Get("https://www.google.com/search?q=" + stock + "+stock&safe=strict&rlz=1C1CHBF_enIE784IE784&sxsrf=ALeKk00R7a6xf6UTpwxc_R23lq_m9yQx0A:1600457550056&gbv=1&sei=TgtlX_eLA9uM1fAP7MacsAc")
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// defer resp.Body.Close()
+		// output, err := ioutil.ReadAll(resp.Body)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// outputstring := string(output)
+		// position := strings.LastIndex(outputstring, "iBp4i")
+		// value := string(outputstring[position+14 : position+25])
+		// for i := 0; i < len(value); i++ {
+		// 	if string(value[i]) == " " {
+		// 		value = value[0:i]
+		// 		break
+		// 	}
+		// }
+		// fmt.Println(stock + ": " + string(value))
 
 	}
+	var waiter sync.WaitGroup
+	waiter.Add(count)
+	for _, i := range name {
+		go func(i string) {
+			request(i)
+			waiter.Done()
+		}(i)
+	}
+	waiter.Wait()
 
-	// if err == nil {
-	// 	help := string(outputstring[0])
-	// 	fmt.Println(help)
-	// 	fmt.Println("HI")
-	// 	// var store int
-	// 	for i := 0; i+6 != len(string(outputstring)); i++ {
-	// 		// store = i
-	// 		if string(outputstring[i:i+5]) == "iBp4i" {
-	// 			fmt.Println(string(outputstring[i-20 : i+20]))
-	// 		}
-	// 	}
-	// 	// answer := string(outputstring[store : store+20])
-	// 	// fmt.Println(answer)
-
-	// }
-
+}
+func request(stock string) {
+	resp, err := http.Get("https://www.google.com/search?q=" + stock + "+stock&safe=strict&rlz=1C1CHBF_enIE784IE784&sxsrf=ALeKk00R7a6xf6UTpwxc_R23lq_m9yQx0A:1600457550056&gbv=1&sei=TgtlX_eLA9uM1fAP7MacsAc")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	output, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	outputstring := string(output)
+	position := strings.LastIndex(outputstring, "iBp4i")
+	value := string(outputstring[position+14 : position+25])
+	for i := 0; i < len(value); i++ {
+		if string(value[i]) == " " {
+			value = value[0:i]
+			break
+		}
+	}
+	fmt.Println(stock + ": " + string(value))
 }
