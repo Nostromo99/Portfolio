6ab82c90d3786067c1019a8cc7e9594126d27d33
aaron <aaronjondorney@gmail.com>
2020-09-21 19:58:17 +0100

    added command line interface

--- Portfolio/starting.go
+++ Portfolio/starting.go
@@ -4,20 +4,65 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 )
 
-//add concurency
-//read in lines and create function to send get requests
 func main() {
+	update()
+	var input string
+	for {
+		fmt.Print("-->")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ = reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			break
+		}
+		if input == "update" {
+			update()
+		} else if len(input) > 4 && input[0:3] == "add" {
+			add(input[4:])
 
+		} else if len(input) > 7 && input[0:6] == "remove" {
+			remove(input[7:])
+		} else {
+			request(input)
+		}
+	}
+
+}
+func request(stock string) {
+	resp, err := http.Get("https://www.google.com/search?q=" + stock + "+stock&safe=strict&rlz=1C1CHBF_enIE784IE784&sxsrf=ALeKk00R7a6xf6UTpwxc_R23lq_m9yQx0A:1600457550056&gbv=1&sei=TgtlX_eLA9uM1fAP7MacsAc")
+	if err != nil {
+		fmt.Println("get request for ticker failed")
+		return
+	}
+	defer resp.Body.Close()
+	output, _ := ioutil.ReadAll(resp.Body)
+
+	outputstring := string(output)
+	position := strings.LastIndex(outputstring, "iBp4i")
+	value := string(outputstring[position+14 : position+25])
+	for i := 0; i < len(value); i++ {
+		if string(value[i]) == " " {
+			value = value[0:i]
+			break
+		}
+	}
+	if _, err := strconv.ParseFloat(value, 64); err != nil {
+		fmt.Println("invalid input")
+	} else {
+		fmt.Println(stock + ": " + value)
+	}
+}
+func update() {
 	tickers, err := os.Open("portfolio/tickers.txt")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("could not access tickers file")
 	}
 	defer tickers.Close()
 	lines := bufio.NewScanner(tickers)
@@ -26,26 +71,6 @@ func main() {
 	for lines.Scan() {
 		name = append(name, lines.Text())
 		count++
-		// stock := lines.Text()
-		// resp, err := http.Get("https://www.google.com/search?q=" + stock + "+stock&safe=strict&rlz=1C1CHBF_enIE784IE784&sxsrf=ALeKk00R7a6xf6UTpwxc_R23lq_m9yQx0A:1600457550056&gbv=1&sei=TgtlX_eLA9uM1fAP7MacsAc")
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// defer resp.Body.Close()
-		// output, err := ioutil.ReadAll(resp.Body)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// outputstring := string(output)
-		// position := strings.LastIndex(outputstring, "iBp4i")
-		// value := string(outputstring[position+14 : position+25])
-		// for i := 0; i < len(value); i++ {
-		// 	if string(value[i]) == " " {
-		// 		value = value[0:i]
-		// 		break
-		// 	}
-		// }
-		// fmt.Println(stock + ": " + string(value))
 
 	}
 	var waiter sync.WaitGroup
@@ -57,26 +82,42 @@ func main() {
 		}(i)
 	}
 	waiter.Wait()
+}
+func add(addition string) {
+	file, err := os.OpenFile("portfolio/tickers.txt", os.O_RDWR, 0755)
+	if err != nil {
+		fmt.Println("could not open tickers file to add entry")
+		return
+	}
+	defer file.Close()
+	lines := bufio.NewScanner(file)
+	match := false
+	for lines.Scan() {
+		if lines.Text() == addition {
+			match = true
+		}
+
+	}
+	if match == false {
+		file.WriteString(addition + "\n")
+		fmt.Println("added " + addition)
+
+	} else {
+		fmt.Println("ticker already in watchlist")
+	}
 
 }
-func request(stock string) {
-	resp, err := http.Get("https://www.google.com/search?q=" + stock + "+stock&safe=strict&rlz=1C1CHBF_enIE784IE784&sxsrf=ALeKk00R7a6xf6UTpwxc_R23lq_m9yQx0A:1600457550056&gbv=1&sei=TgtlX_eLA9uM1fAP7MacsAc")
-	if err != nil {
-		log.Fatal(err)
+func remove(removal string) {
+	file, _ := ioutil.ReadFile("portfolio/tickers.txt")
+	filestring := string(file)
+	fmt.Println(filestring)
+	replacement := strings.ReplaceAll(filestring, removal+"\n", "")
+	if filestring == replacement {
+		fmt.Println(removal + " not in watchlist")
+		return
+
 	}
-	defer resp.Body.Close()
-	output, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	outputstring := string(output)
-	position := strings.LastIndex(outputstring, "iBp4i")
-	value := string(outputstring[position+14 : position+25])
-	for i := 0; i < len(value); i++ {
-		if string(value[i]) == " " {
-			value = value[0:i]
-			break
-		}
-	}
-	fmt.Println(stock + ": " + string(value))
+	ioutil.WriteFile("portfolio/tickers.txt", []byte(replacement), 0644)
+	fmt.Println("removed " + removal)
+
 }
